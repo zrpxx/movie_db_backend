f6cd6a928d7613e4f6d953bebcbabd183c7a9a31
Zhangrp <zrpxx@qq.com>
2021-12-16 14:02:58 +0800

    Added category_id list filter for movies

--- api/views.py
+++ api/views.py
@@ -25,11 +25,13 @@ class MovieViewSet(viewsets.ModelViewSet):
         queryset = queryset.prefetch_related('directors')
         queryset = queryset.prefetch_related('categories')
         movie_id = self.request.query_params.get('id', None)
-        movie_category_id = self.request.query_params.get('category_id', None)
+        movie_category_ids = self.request.query_params.getlist('category_id[]', None)
         if movie_id is not None:
             queryset = queryset.filter(id=movie_id)
-        if movie_category_id is not None:
-            queryset = queryset.filter(categories__in=[movie_category_id])
+        if movie_category_ids is not None:
+            print(movie_category_ids)
+            for movie_category_id in movie_category_ids:
+                queryset = queryset.filter(categories__id=movie_category_id)
         return queryset
 
     def create(self, request, *args, **kwargs):
